be5b80c532b17c2b8e597455b6211ef333fe1dc6
Dmitry Gordashnikov <dmitry.g.gordashnikov@gmail.com>
2022-10-30 16:08:23 +0200

    Create header section and record fields in fragment_running.xml

--- app/src/main/java/com/rustyrobot/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/rustyrobot/recordkeeper/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.rustyrobot.recordkeeper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.rustyrobot.recordkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_cycling -> {
+                    onCyclingClicked()
+                    true
+                }
+                R.id.nav_running -> {
+                    onRunningClicked()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+    private fun onRunningClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+
+    }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+
+    }
+
+
 }
